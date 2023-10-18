b2d2393fbb4ff5459ff50f925e63aa4b498b646b
Adriana Silva <ataysilva8801@gmail.com>
2023-10-18 10:31:49 -0400

    Initial Commit

---
+++ Story Template/Chapter 3/Chapter3.swift
@@ -0,0 +1,13 @@
+//
+//  Chapter3.swift
+//  CLIStoryTemplate
+//
+
+import Foundation
+
+
+func chapterThree() {
+    // Your portion of the story goes here
+    print("After seeing the shadow the little girl ran for the stairs only for the door to be locked. The little girl started crying and pounding on the door when all of the sudden it opened on its own. The little girl ran to her room & swore to never go into the basement again.")
+    
+}
