7361424a871b2fc4e52bc9414524c516c0a811e1
Arriel Jones <Jonesarriel11@gmail.com>
2023-10-18 10:31:49 -0400

    Initial Commit

---
+++ Story Template/Chapter 2/Chapter2.swift
@@ -0,0 +1,13 @@
+//
+//  Chapter2.swift
+//  CLIStoryTemplate
+//
+
+import Foundation
+
+func chapterTwo() {
+    // Your portion of the story goes here
+    print("The Noise")
+    print("While she was in the basement she heard a noise. She walked near the sound and saw a fast shadow run by.")
+    }
+    
